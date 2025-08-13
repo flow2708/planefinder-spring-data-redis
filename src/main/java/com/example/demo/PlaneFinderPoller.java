72ae51276b18f44e207c645e423c5e8f8c5485d5
flow2708 <dibirovamin10@gmail.com>
2025-08-13 22:13:40 +0300

    Update PlaneFinderPoller.java

--- src/main/java/com/example/demo/PlaneFinderPoller.java
+++ src/main/java/com/example/demo/PlaneFinderPoller.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisOperations;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 
@@ -17,5 +18,22 @@ public class PlaneFinderPoller {
                       RedisOperations<String, Aircraft> redisOperations) {
         this.connectionFactory = connectionFactory;
         this.redisOperations = redisOperations;
+    }
+    @Scheduled(fixedRate = 1000)
+    private void pollPlanes() {
+        connectionFactory.getConnection().serverCommands().flushDb();
+
+        client.get()
+                .retrieve()
+                .bodyToFlux(Aircraft.class)
+                .filter(plane -> !plane.getReg().isEmpty())
+                .toStream()
+                .forEach(ac -> redisOperations.opsForValue().set(ac.getReg(), ac));
+
+        redisOperations.opsForValue()
+                .getOperations()
+                .keys("*")
+                .forEach(ac ->
+                        System.out.println(redisOperations.opsForValue().get(ac)));
     }
 }
